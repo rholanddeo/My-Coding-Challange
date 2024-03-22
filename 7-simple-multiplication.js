141ca1601dc9718af64ca8266e1eb17910f3293e
rholanddeo <rholanddeo210@gmail.com>
2024-03-22 09:10:22 +0700

    solved: Fri 22 Mar 09.10

---
+++ 7-simple-multiplication.js
@@ -0,0 +1,9 @@
+//https://www.codewars.com/kata/583710ccaa6717322c000105/train/javascript
+
+// function simpleMultiplication(number) {
+//     return number % 2 === 0 ? number * 8 : number * 9;
+// }
+
+const simpleMultiplication = number => number % 2 === 0 ? number * 8 : number * 9;
+
+console.log(simpleMultiplication(2));

efce70cbc105500b295b55ca109651278c2a7743
rholanddeo <rholanddeo210@gmail.com>
2024-03-22 09:18:05 +0700

    solved: Fri 22 mar 09.18

---
+++ 9-basic-math-op.js
@@ -0,0 +1,9 @@
+//https://www.codewars.com/kata/57356c55867b9b7a60000bd7/train/javascript
+
+// function basicOp(operation, value1, value2) {
+//     return eval(value1 + operation + value2);
+// }
+
+const basicOp = (operation, value1, value2) => eval(value1 + operation + value2);
+
+console.log(basicOp('-', 4, 7));

547337a55d613d791476be6688a28265413b6c98
rholanddeo <rholanddeo210@gmail.com>
2024-03-22 09:15:00 +0700

    solved: Fri 22 Mar 09.14

---
+++ 8-reversed-string.js
@@ -0,0 +1,9 @@
+//https://www.codewars.com/kata/5168bb5dfe9a00b126000018/train/javascript
+
+// function solution(str){
+//     return str.split('').reverse().join('');
+// }
+
+const solution = str => str.split('').reverse().join('');
+
+console.log(solution('world'));

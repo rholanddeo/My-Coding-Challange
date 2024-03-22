6dae2c4f24e76371ef999ec9366cacaeb130bc58
rholanddeo <rholanddeo210@gmail.com>
2024-03-22 09:20:29 +0700

    solved: Fri 22 Mar 09.20

---
+++ 10-find-max-min.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
+
+// function findMaxMin(arr){
+//     return [Math.min(...arr), Math.max(...arr)];
+// }
+
+const findMaxMin = arr => [Math.min(...arr), Math.max(...arr)];
+
+console.log(findMaxMin([1,2,3,4,5]));
